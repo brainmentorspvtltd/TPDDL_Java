b5d3f06551655596644f6e66cf1f2feb180560a1
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2019-05-02 12:23:25 +0530

    Code

---
+++ FirstClass.java
@@ -0,0 +1,16 @@
+// Class start with capital letter (Convention)
+ class Add{
+    // main is the entry point of a program
+    static public  void main(int x){
+        System.out.println("My Main ");
+    }
+    static public  void main(String args[]){
+        System.out.println("Addition is");
+       // main();
+        /*int first = Integer.parseInt(arr[0]);
+        int second = Integer.parseInt(arr[1]);
+        int result = first + second;
+        System.out.println("Addition is  "+result);
+        */
+    }
+}
